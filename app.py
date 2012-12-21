6d7be6344b9a34c4c8ec9781db14d071dcc9ee21
qingfeng <qingfeng@douban.com>
2012-12-21 22:03:33 +0800

    完成文件上传

--- app.py
+++ app.py
@@ -1,4 +1,43 @@
-# a dummy wsgi app
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ["hello, world!"]
+import os
+import uuid
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+from dae.api import permdir
+
+UPLOAD_FOLDER = permdir.get_permdir()
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def gen_filename():
+    return "%s.jpg" % uuid.uuid1().hex
+
+@app.route("/i")
+def i():
+    return "Hello World!"
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = gen_filename()
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "http://p.dapps.douban.com/i/%s" % filename
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == "__main__":
+    app.run()
