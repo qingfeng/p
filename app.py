7ebb1c949454173d6a91cf8c5797e93a80d69b00
qingfeng <qingfeng@douban.com>
2012-12-23 18:26:08 +0800

    add resize image example

--- app.py
+++ app.py
@@ -53,8 +53,9 @@ def hello():
     return '''
     <!doctype html>
     <title>Upload new File</title>
-    Command line: `curl -F file=@"/tmp/1.png" http://p.dapps.douban.com/`<br>
-    Command line: `curl -F file=@"/tmp/1.png" -F w=100 -F h=100 http://p.dapps.douban.com/`<br>
+    Command line: ``curl -F file=@"/tmp/1.png" http://p.dapps.douban.com/``<br>
+    Command line: ``curl -F file=@"/tmp/1.png" -F w=100 -F h=100 http://p.dapps.douban.com/``<br>
+    Resize image: ``http://p.dapps.douban.com/r/img_hash.jpg?w=300&h=200``<br>
     <form action="" method=post enctype=multipart/form-data>
       <p><input type=file name=file>
          w:<input type=text name=w>
